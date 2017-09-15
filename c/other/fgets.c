1323e9cea391f60e7870452ba90c0c33c6a3dabe
fangzhf <fangzhf@inspur.com>
2017-09-15 23:15:52 +0800

    use fscanf to replace fgets

--- c/other/fgets.c
+++ c/other/fgets.c
@@ -5,21 +5,17 @@ int main (void){
 
     char buf[32] = {0};
 	FILE *fp = fopen("udc_info.txt", "r");
+    int count = 0;
     
     if (fp){
-        while(1){
-            fgets(buf, sizeof(buf), fp);
-
-            if (feof(fp)){
-                break;
-            }
-
-            if (buf[strlen(buf) - 1] == '\n'){
-                buf[strlen(buf) - 1] = '\0';
-            }
+        while(fscanf(fp, "%s", buf) != EOF){
+            count ++;
             printf("buf is %s\n", buf);
             memset(buf, 0, sizeof (buf));
         }
+        printf("%d\n", count);
+        fclose(fp);
+        fp = NULL;
     }
 	return 0;
 }
