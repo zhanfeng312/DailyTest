d547a7436c0212a41b0eeacb2e1e3a0c972e10ca
fangzhf <fangzhf@inspur.com>
2017-10-29 22:47:55 +0800

    optimized hello_world.c

--- c/hello_world.c
+++ c/hello_world.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>		/* not strings.h !!! */
+#include <string.h>     /* not strings.h !!! */
 #include <stdarg.h>
 #include <time.h>
 
@@ -17,27 +17,27 @@
 #include <sys/select.h>
 #include <sys/stat.h>
 #include <sys/un.h>
-#include <sys/wait.h>		/* WEXITSTATUS */
-#include <linux/sockios.h>	/* TIOCOUTQ */
+#include <sys/wait.h>       /* WEXITSTATUS */
+#include <linux/sockios.h>  /* TIOCOUTQ */
 #include <pthread.h>
 #include <signal.h>
 
 
 #define PROG_NAME   "daemon"
 
-void		usage();
-static void	sig_handle(int sig);
-int			prog_exit(int eno);
-void*		worker_thread(void* arg);
-void*		event_thread(void* arg);
+void        usage();
+static void sig_handle(int sig);
+int         prog_exit(int eno);
+void*       worker_thread(void* arg);
+void*       event_thread(void* arg);
 
 
 
 /*********************************************************************************
 * Global Variables
 */
-int		g_debug = 0;
-int		g_system_done = 0;
+int     g_debug = 0;
+int     g_system_done = 0;
 /********************************************************************************/
 
 
@@ -46,25 +46,25 @@ int		g_system_done = 0;
 int main(int argc, char* argv[])
 {
     //signal deal start
-	struct sigaction sa;
-	sa.sa_flags = 0;
-	
-	sa.sa_handler = SIG_IGN;
-	sigaction (SIGCHLD, &sa, NULL);/* WIFEXITED and WEXITSTATUS after system need SIGCHLD */
-	sa.sa_handler = SIG_IGN;
-	sigaction (SIGPIPE, &sa, NULL);
-	sa.sa_handler = sig_handle;
-	sigaction (SIGHUP, &sa, NULL);
-	sa.sa_handler = sig_handle;
-	sigaction (SIGQUIT, &sa, NULL);
-	sa.sa_handler = sig_handle;
-	sigaction (SIGTERM, &sa, NULL);/* killall */
-	sa.sa_handler = sig_handle;
-	sigaction (SIGSTOP, &sa, NULL);
-	sa.sa_handler = sig_handle;
-	sigaction (SIGINT, &sa, NULL);
+    struct sigaction sa;
+    sa.sa_flags = 0;
+    
+    sa.sa_handler = SIG_IGN;
+    sigaction (SIGCHLD, &sa, NULL);/* WIFEXITED and WEXITSTATUS after system need SIGCHLD */
+    sa.sa_handler = SIG_IGN;
+    sigaction (SIGPIPE, &sa, NULL);
+    sa.sa_handler = sig_handle;
+    sigaction (SIGHUP, &sa, NULL);
+    sa.sa_handler = sig_handle;
+    sigaction (SIGQUIT, &sa, NULL);
+    sa.sa_handler = sig_handle;
+    sigaction (SIGTERM, &sa, NULL);/* killall */
+    sa.sa_handler = sig_handle;
+    sigaction (SIGSTOP, &sa, NULL);
+    sa.sa_handler = sig_handle;
+    sigaction (SIGINT, &sa, NULL);
     //signal deal end
-	
+    
     //parse cmd start
     int c = 0;
     while ((c = getopt(argc, argv, "hd")) != EOF)
@@ -107,43 +107,43 @@ int main(int argc, char* argv[])
 
     //thread start
     pthread_t tid_worker_thread;
-	pthread_t tid_event_thread;
-	pthread_create(&tid_worker_thread, NULL, worker_thread, NULL);
-	pthread_create(&tid_event_thread, NULL, event_thread, NULL);
+    pthread_t tid_event_thread;
+    pthread_create(&tid_worker_thread, NULL, worker_thread, NULL);
+    pthread_create(&tid_event_thread, NULL, event_thread, NULL);
 
-	while (0 == g_system_done)
-	{
-		sleep(1);
-	}
+    while (0 == g_system_done)
+    {
+        sleep(1);
+    }
 
-	pthread_join(tid_worker_thread, NULL);
-	pthread_join(tid_event_thread, NULL);
+    pthread_join(tid_worker_thread, NULL);
+    pthread_join(tid_event_thread, NULL);
     //thread end
     
-	prog_exit(0);
-	return 0;
+    prog_exit(0);
+    return 0;
 }
 
 void *worker_thread(void* arg)
 {
-	if (g_debug) puts("worker_thread start ...");
-	while (0 == g_system_done)
-	{
-		sleep(1);
-	}
-	if (g_debug) puts("worker_thread end ...");
-	pthread_exit (NULL);
+    if (g_debug) puts("worker_thread start ...");
+    while (0 == g_system_done)
+    {
+        sleep(1);
+    }
+    if (g_debug) puts("worker_thread end ...");
+    pthread_exit (NULL);
 }
 
 void *event_thread(void* arg)
 {
-	if (g_debug) puts("event_thread start ...");
-	while (0 == g_system_done)
-	{	
-		sleep(1);
-	}
-	if (g_debug) puts("event_thread end ...");
-	pthread_exit (NULL);
+    if (g_debug) puts("event_thread start ...");
+    while (0 == g_system_done)
+    {   
+        sleep(1);
+    }
+    if (g_debug) puts("event_thread end ...");
+    pthread_exit (NULL);
 }
 
 
@@ -155,27 +155,27 @@ void usage()
 
 static void sig_handle(int sig)
 {
-	switch (sig)
-	{
-    	case SIGPIPE:
-    		puts("");
-    		break;
-    	case SIGHUP:
-    		puts("");
-    		break;
-    	case SIGINT:
-    	case SIGTERM:
-    	case SIGQUIT:
-    	case SIGSTOP:
-    		puts("");
-    		if (g_debug) puts(PROG_NAME" end ...");
-    		g_system_done = 1;
-    		break;
-	}
+    switch (sig)
+    {
+        case SIGPIPE:
+            puts("");
+            break;
+        case SIGHUP:
+            puts("");
+            break;
+        case SIGINT:
+        case SIGTERM:
+        case SIGQUIT:
+        case SIGSTOP:
+            puts("");
+            if (g_debug) puts(PROG_NAME" end ...");
+            g_system_done = 1;
+            break;
+    }
 }
 
 
 int prog_exit(int eno)
-{	
-	exit (eno);
+{   
+    exit (eno);
 }
