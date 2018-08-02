a7abe84c621ce23ac561d10c8ccf7d09f1064bcb
fangzhf <fangzhf@inspur.com>
2018-08-02 09:02:12 +0800

    shell: add check_interface_link_speed.sh

---
+++ Shell/check_interface_link_speed.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+ETHTOOL="/root/bin/ethtool"
+INTERFACE_NAME="eth0"
+MATCH_PATTERN="Speed: 10Mb/s"
+IFCONFIG="/sbin/ifconfig"
+DOWN_UP_INTERVAL_TIME=1
+SLEEP_TIME=2
+
+while true
+do	
+	${ETHTOOL} ${INTERFACE_NAME} | grep "${MATCH_PATTERN}" 1>/dev/null 2>/dev/null
+	
+	if [ $? -eq 0 ]; then
+	
+		${IFCONFIG} ${INTERFACE_NAME} down
+		sleep ${DOWN_UP_INTERVAL_TIME}
+		${IFCONFIG} ${INTERFACE_NAME} up
+		${ETHTOOL} -s ${INTERFACE_NAME} autoneg on
+	fi
+	sleep ${SLEEP_TIME}
+done
