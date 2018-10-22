eefb29cdf4defe34e1a5557100e6cb204d8f53fd
fangzhf <fangzhf@inspur.com>
2018-10-22 08:47:40 +0800

    update test_var Makefile;IO code add sscanf

--- C/IO/test_input_output.c
+++ C/IO/test_input_output.c
@@ -77,7 +77,38 @@ void test_scanf(void)
 
 void test_sscanf(void)
 {
-
+#if 0
+	1、一般用法
+		char buf[512] = { 0 };
+		sscanf("123456 ", "%s", buf);
+		printf("%s\n", buf);
+		结果为：123456
+	2. 取指定长度的字符串。如在下例中，取最大长度为4字节的字符串。
+		sscanf("123456 ", "%4s", buf);
+		printf("%s\n", buf);
+		结果为：1234
+	3. 取到指定字符为止的字符串。如在下例中，取遇到空格为止字符串。
+		sscanf("123456 abcdedf", "%[^ ]", buf);
+		printf("%s\n", buf);
+		结果为：123456
+	4. 取仅包含指定字符集的字符串。如在下例中，取仅包含1到9和小写字母的字符串。
+		sscanf("123456abcdedfBCDEF", "%[1-9a-z]", buf);
+		printf("%s\n", buf);
+		结果为：123456abcdedf
+	5. 取到指定字符集为止的字符串。如在下例中，取遇到大写字母为止的字符串。
+		sscanf("123456abcdedfBCDEF", "%[^A-Z]", buf);
+		printf("%s\n", buf);
+		结果为：123456abcdedf
+	6、给定一个字符串iios / 12DDWDFF@122，获取 / 和 @ 之间的字符串，先将 "iios/"过滤掉，再将非'@'的一串内容送到buf中
+		sscanf("iios/12DDWDFF@122", "%*[^/]/%[^@]", buf);
+		printf("%s\n", buf);
+		结果为：12DDWDFF
+	7、给定一个字符串"hello, world"，仅保留"world"。（注意：“，”之后有一空格）
+		sscanf("hello, world", "%*s%s", buf);
+		printf("%s\n", buf);
+		结果为：world
+	PS. %*s表示第一个匹配到的%s被过滤掉，即hello被过滤了，如果没有空格则结果为NULL。
+#endif
 }
 
 int main(int argc, char *argv[])
