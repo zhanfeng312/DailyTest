c96df7f183601546260d212869f4580e1f79a8a4
fangzhf <fangzhf@inspur.com>
2019-06-18 19:49:33 +0800

    remove docker into jianguoyun

--- docker/Ubuntu1404/1404_V0200/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-IMAGE_NAME=ubuntu:1404_V0200
-
-docker image rm $IMAGE_NAME
-docker build -t $IMAGE_NAME ./dev
--- docker/Ubuntu1404/1404_V0300_lingzhi/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-IMAGE_NAME=ubuntu:1404_V0300_lingzhi
-
-docker image rm $IMAGE_NAME
-docker build -t $IMAGE_NAME ./dev
