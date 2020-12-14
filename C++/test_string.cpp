0f4f379c3837a1ec368b2c00f59ce506feb77092
fangzf0705 <fangzf0705@thundersoft.com>
2020-12-14 13:20:17 +0800

    add test_string

---
+++ C++/test_string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+//using std::string;
+using namespace std;
+
+int main() {
+    string a("1111");
+    cout << a << a.c_str() << endl;
+    
+    return 0;
+}
