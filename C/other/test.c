a1df9a9c0803c1d7152d229214766b67e562e6f4
fangzf0705 <fangzf0705@thundersoft.com>
2020-12-17 23:09:38 +0800

    完善test.c

--- C/other/test.c
+++ C/other/test.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
+#include "../common/nt_common.h"
 #include <stdint.h>
 
 void test_yiwei(void)
 {
 #if 0
+    移位的位数范围为0 ~ 31
     1. 对于常量，由于左移的位数大于了31位，所以结果为0
-    2. 对于变量，如果移位的位数b大于了该类型的大小sz ，则先用b对sz取模，即 b = b % sz，然后再进行移位运算
+    2. 对于变量，如果移位的位数b大于31位，将所移动的位数除以32取得的余数即为移位的位数
 #endif
 
-    int a = 1;
-    int b = 32;
-    MY_LOG("%d %d", a << b, 1 << 31);
+    int a = 1, b = 33;
+    MY_LOG("%d", a << b); //实际移位1位, 结果为2
+    MY_LOG("%d", 1 << 33); //变量左移, 结果为0
 
     int i = 0xffffffff;
     MY_LOG("%d的二进制形式为:0x%08x", i, i);
-
+    
 #if 0
     左移:当为有符号数时,符号位会被移出
     因为0x80000000 的二进制形式为:
     10000000 00000000 00000000 00000000, 左移一位后变为:
     00000000 00000000 00000000 00000000
 #endif
-
     i = 0x80000000;
     MY_LOG("[0x%08x]左移一位为:[0x%08x]", i, i << 1);
 
@@ -37,6 +38,7 @@ void test_yiwei(void)
 
 void test_other(void)
 {
+#if 0
     typedef struct {
         uint16_t len;
         char data[0];
@@ -49,14 +51,25 @@ void test_other(void)
     MY_LOG("sizeof(p) = %ld", sizeof(p));
 
     char p1[] = "";
-    MY_LOG("p1 = %p:&p1 = %p", p1, &p1);
+    MY_LOG("p1 = %p:&p1 = %p", p1, &p1); //字符数组, p1就是数组的地址，值和&p1相同
+#endif
+
+    const char *buf = "123";
+    uint8_t a = buf[0];
+    printf("a = %u\n", a);
+    a = '0';
+    printf("a = %u\n", a);
+    a = '9';
+    printf("a = %u\n", a);
+    a = 0xEF;
+    printf("a = %u\n", a);
 }
 
 int main(void)
 {
-    printf("CompileTime:%s-%s-%s-%d\n", __DATE__, __TIME__, __func__, __LINE__);
+    printf("CompileTime: [%s-%s]\n", __DATE__, __TIME__);
     test_yiwei();
-    // test_other();
+    test_other();
 
     return 0;
 }
