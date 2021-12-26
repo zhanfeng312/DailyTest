d08473c75c67343d639b7d04d5420422bcb4d935
fangzhf <fangzhf@inspur.com>
2021-12-26 11:48:04 +0800

    fix var_replace.sh

--- Shell/var_replace.sh
+++ Shell/var_replace.sh
@@ -14,6 +14,16 @@ var="Prefix"
 echo ${var:+"This is default value"}
 echo "4 - Value of var is $var"
 
-
 echo ${var:?"Print this message"}
 echo "5 - Value of var is ${var}"
+
+# Variable is not set
+# 1 - Value of var is
+# Variable is not set
+# 2 - Value of var is Variable is not set
+
+# 3 - Value of var is
+# This is default value
+# 4 - Value of var is Prefix
+# Prefix
+# 5 - Value of var is Prefix
