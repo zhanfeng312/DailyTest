4e58e641f97a0a0450249dd8ba4d88c7b6509ae1
fangzhf <fangzhf@inspur.com>
2022-01-02 11:24:32 +0800

    fix process env code

--- C/process/src/process_env.c
+++ C/process/src/process_env.c
@@ -2,29 +2,16 @@
 
 /* environ变量是一个char** 类型，存储着系统的环境变量 */
 extern char **environ;
-void ShowEnv(void)
-{
-    printf("环境表:\n");
-    int i = 0;
-    char *env = NULL;
-    while ((env = environ[i]) != NULL) {
-        printf("%10s\n", env);
-        i++;
-    }
-}
-
 /*
     进程启动时，都有一个独立的环境表，初始的环境表继承自父进程
     环境表本质就是一个指针数组，遍历数组即可
 */
 int main(int argc, char *argv[])
 {
-    printf("主函数参数列表:\n");
     int i = 0;
-    for (i = 0; i < argc; i++) {
-        printf("%10s\n", argv[i]);
+    for (i = 0; environ[i] != NULL; i++) {
+        printf("%s\n", environ[i]);
     }
-    ShowEnv();
 
     return 0;
 }
